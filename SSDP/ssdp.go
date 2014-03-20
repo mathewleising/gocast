c6fafd1e99834f11cefd7802d6895e2dc35a496a
Mathew Leising <mathewleising@Mathew-Leisings-MacBook-Pro.local>
2014-03-19 21:47:13 -0400

    Cleaned up main mostly. Added flags also.

--- SSDP/ssdp.go
+++ SSDP/ssdp.go
@@ -3,7 +3,6 @@ package ssdp
 import(
     "fmt"
     "net"
-    "os"
     "net/http"
     "io"
     "strings"
@@ -11,7 +10,6 @@ import(
 
 const (
     ssdpListen string = "239.255.255.250:1900"
-    //ssdpReplyTemplate string = `HTTP/1.1 200 OK\r\nLOCATION: http://%s:%d/dd.xml\r\nCACHE-CONTROL: max-age=1800\r\nEXT:\r\nBOOTID.UPNP.ORG: 1\r\nSERVER: Linux/2.6 UPnP/1.0 quick_ssdp/1.0\r\nST: urn:dial-multiscreen-org:service:dial:1\r\nUSN: uuid:%s::urn:dial-multiscreen-org:service:dial:1\r\n\r\n`
     ssdpReplyTemplate string =  `HTTP/1.1 200 OK
 LOCATION: http://%s:%d/dd.xml
 CACHE-CONTROL: max-age=1800
@@ -20,7 +18,6 @@ BOOTID.UPNP.ORG: 7337
 USN: uuid:%s
 ST: urn:dial-multiscreen-org:service:dial:1
 `
-
     ddxmlTemplate string = `HTTP/1.1 200 OK
 Content-Type: application/xml
 Application-URL: http://%s:%d/apps/
@@ -99,6 +96,7 @@ func (s *Server) handleMessage(){
 
     n, addr, err := s.conn.ReadFromUDP(b)
     if err != nil || n == 0{
+        fmt.Printf("Oops ReadFromUDP Error: %v\n", err)
         continue
     }
 
@@ -110,7 +108,7 @@ func (s *Server) handleMessage(){
       ssdpReply := fmt.Sprintf(ssdpReplyTemplate, s.httpAddr, s.httpPort, s.httpAddr, s.httpPort, s.uuid)
       _,err := s.conn.WriteToUDP([]byte(ssdpReply),addr)
       if err != nil {
-        fmt.Println(err) 
+        fmt.Printf("Oops WriteToUDP Error: %v\n", err)
       } else {
         fmt.Printf("Responded Successfully.\n")
       }
@@ -118,23 +116,7 @@ func (s *Server) handleMessage(){
     }
 }
 
-/*func (s *Server) sendMessage(c Client) {
-    ddXML= fmt.Sprintf(ddxmlTemplate, ip, port, uuid, "GiveUsAnAPlus")
-    for{
-        n,err := s.conn.WriteToUDP([]byte(ddXML),c.userAddr)
-        fmt.Println(n,err)
-    }
-
-}*/
-
 func returnDeviceDescriptionXML(w http.ResponseWriter, req *http.Request, ddxml string) {
     fmt.Println(req)
     io.WriteString(w, ddxml)
-}
-
-func checkError(err error){
-    if err != nil{
-        fmt.Fprintf(os.Stderr,"Fatal error:%s",err.Error())
-        os.Exit(1)
-    }
 }
--- main.go
+++ main.go
@@ -1,40 +1,73 @@
 package main
 
 import (
-    "log"
     "strings"
     "github.com/mathewleising/gocast/SSDP"
     "os/exec"
     "net"
     "fmt"
-    "os"
+    "flag"
 )
 
+var (
+    address string
+    port int
+    uuid string
+)
+
+func init() {
+    flag.StringVar(&address, "address", getLocalIP(), "IPv4 address for device description, default is local IP")
+    flag.IntVar(&port, "port", 1234, "UPnP port for device description, Default is 1234, which is also my default password, shhh")
+    flag.StringVar(&uuid, "uuid", getUUID(), "UUID, it's used for something?... Default uses OS UUID, hope you don't mind :)")
+}
+
+
 func main() {
-    name, err := os.Hostname()
-    if err != nil {
-        fmt.Printf("Oops: %v\n", err)
-        return
-    }
+    defer func() {
+        if r := recover(); r != nil {
+            fmt.Println("Please forgived me, I panicked :|\n", r)
+        }
+    }()
 
-    addrs, err := net.LookupHost(name)
+    s,err := ssdp.NewSSDPServer(address, port, uuid)
     if err != nil {
-        fmt.Printf("Oops: %v\n", err)
-        return
-    }
-
-    fmt.Println(addrs[0])
-
-    uuid, err := exec.Command("uuidgen").Output()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    strUuid := strings.Replace(string(uuid), "\n", "", -1)
-    s,err := ssdp.NewSSDPServer(addrs[0], 1234, strUuid)
-    if err != nil {
-        log.Fatal(err)
+        panic(fmt.Sprintf("Oops NewSSDPServer Error: %v\n", err))
     }
 
     s.Run()
+}
+
+func getLocalIP() string {
+    tt, err := net.Interfaces() 
+    if err != nil { 
+        panic(fmt.Sprintf("Oops uuidgen Error: %v\n", err))
+    } 
+
+    for _, t := range tt { 
+        aa, err := t.Addrs() 
+        if err != nil { 
+            panic(fmt.Sprintf("Oops uuidgen Error: %v\n", err))
+        } 
+        for _, a := range aa { 
+            ipnet, ok := a.(*net.IPNet) 
+            if !ok { 
+                continue 
+            } 
+            v4 := ipnet.IP.To4() 
+            if v4 == nil || v4[0] == 127 { // loopback address 
+                continue 
+            }
+            return string(v4)
+        } 
+    } 
+    panic(fmt.Sprintf("Well this is embarrassing.. You started this program without having any IP Addresses open.. Come back when you are ready\n"))
+}
+
+func getUUID() string {
+    uuid, err := exec.Command("uuidgen").Output()
+    if err != nil {
+        panic(fmt.Sprintf("Oops uuidgen Error: %v\n", err))
+    }
+
+    return strings.Replace(string(uuid), "\n", "", -1)
 }
